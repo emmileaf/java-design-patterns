7f01aafde163d8854372cd506404649a81694ed7
Emily Wang <emilywang@live.ca>
2021-11-16 14:25:24 -0500

    Updated expected behaviour to use logger outputs

--- pessimistic-lock/src/main/java/com/iluwatar/pessimisticlock/App.java
+++ pessimistic-lock/src/main/java/com/iluwatar/pessimisticlock/App.java
@@ -16,22 +16,17 @@ public class App {
             BookNotFoundException,
             LockException {
 
-        // TODO: End-to-end test scenario below - move to a different location later?
+        // TODO: End-to-end test scenario below - incorporate into test suite later
 
         // bookRepository represents a simple database
         BookRepository bookRepository = new BookRepository();
-        // consider a repository of two book objects
+        // consider a repository of one book object
         long book1Id = 1;
-        long book2Id = 2;
         Book book1 = new Book();
-        Book book2 = new Book();
         book1.setId(book1Id);
         book1.setTitle("The Hobbit");
-        book2.setId(book2Id);
-        bookRepository.add(book1); // adding a book with empty title and author
+        bookRepository.add(book1); // adding a book and pre-setting its title
         LOGGER.info("A book with id {} and title {} was added to repository", book1.getId(), book1.getTitle());
-        bookRepository.add(book2); // adding a book with empty title and author
-        LOGGER.info("An empty book with id {} was added to repository", book2.getId());
 
         // set up a SessionManager to let user sessions access the book repository
         var sessionManager = new SessionManager(bookRepository);
@@ -42,31 +37,51 @@ public class App {
 
         // Alice and Bob try to operate on books concurrently
         new Thread(() -> {
+            LOGGER.info("Alice initiated WRITE operation on book {}.", book1.getId());
             Boolean written = sessionManager.write(aliceSession, book1Id, "Title", "Harry Potter");
-            assert(written == true);
+            if (written == true) {
+                LOGGER.info("Alice performed WRITE operation on book {}.", book1.getId());
+            } else {
+                LOGGER.error("Alice is unable to perform WRITE operation on book {}.", book1.getId());
+            }
         }).start();
 
         new Thread(() -> {
             // read operation should fail to acquire lock
+            LOGGER.info("Bob initiated READ operation on book {}.", book1.getId());
             String newTitle = sessionManager.read(bobSession, book1Id, "Title"); // should fail to acquire lock
-            assert(newTitle == null);
+            if (newTitle == null) {
+                LOGGER.info("Bob is unable to perform READ on book {} while Alice is editing.", book1.getId());
+            } else {
+                LOGGER.error("Bob performed READ on book {}, but shouldn't be allowed while Alice edits.", book1.getId());
+            }
         }).start();
 
         new Thread(() -> {
             // write operation should fail to acquire lock
+            LOGGER.info("Bob initiated WRITE operation on book {}.", book1.getId());
             Boolean written = sessionManager.write(bobSession, book1Id, "Title", "Watership Down");
-            assert(written == false);
+            if (written == false) {
+                LOGGER.info("Bob is unable to perform WRITE on book {} while Alice is editing.", book1.getId());
+            } else {
+                LOGGER.error("Bob can perform WRITE on book {}, but should not be while Alice edits.", book1.getId());
+            }
         }).start();
 
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
-            LOGGER.info("Time stall interrupted");
+            LOGGER.error("Time stall of two seconds was interrupted");
         }
 
         new Thread(() -> {
+            LOGGER.info("Bob initiated READ operation on book {}.", book1.getId());
             String newTitle = sessionManager.read(bobSession, book1Id, "Title"); // should succeed to read
-            assert(newTitle == "Harry Potter");
+            if (newTitle == "Harry Potter") {
+                LOGGER.info("Bob is able perform READ on book {} and fetch the title updated by Alice.", book1.getId());
+            } else {
+                LOGGER.error("Bob did not fetch the correct title for on book {} updated by Alice.", book1.getId());
+            }
         }).start();
 
     }
